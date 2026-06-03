fcc6dbf360b33145564a0303607aff3cb657c29c
Kakarotto <shizw@ziroom.com>
2026-06-03 18:54:59 +0800

    to: 修改文件名

--- packages/components/vicon/index.ts
+++ packages/components/vicon/index.ts
@@ -1,4 +1,4 @@
-import _Icon from './Icon.vue'
+import _Icon from './vicon.vue/index.js'
 import { withInstall } from '@kunlun/utils/install'
 
 export const Icon = withInstall(_Icon)
--- packages/components/vicon/vicon.test.tsx
+++ packages/components/vicon/vicon.test.tsx
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import { Icon } from './index'
-import IconComponent from './Icon.vue'
+import IconComponent from './vicon.vue/index.js'
 
 describe('Icon', () => {
   it('renders correctly', () => {
